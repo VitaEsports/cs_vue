13eb495454d2726e6e69350d63a04d06fe09e059
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-24 21:58:55 +0100

    updated

--- public/src/main1.js
+++ public/src/main1.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App, { message: 'Hello from Vue.js!' } )
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
