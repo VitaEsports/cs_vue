31261a6bbc846dd8ff6c33d80e453bcd109fd085
Letronik <vitaesportsgroup@gmail.com>
2024-03-24 19:28:00 +0100

    main folder change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import PlayerMainArea from './components/overlay1/player/PlayerMainArea.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <PlayerMainArea />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ src/components/overlay1/player/PlayerImage.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <img src="@/assets/player_empty.svg" loading="lazy" data-hudid="player_1_image" alt="" class="player_image">
+</template>
+
+<style>
+    .player_image {
+        z-index: 2;
+        width: 80%;
+        position: absolute;
+        bottom: 58%;
+  }
+</style>
