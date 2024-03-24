a2586e0a11457a1177f66eb935c621ae795ec5b4
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-24 23:07:44 +0100

    fixed

---
+++ src/App.vue
@@ -0,0 +1,145 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import PlayerAlive from './components/overlay1/player/PlayerAlive.vue'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <body>
+    <div class="vita_v1">
+      <div class="hud_top_wrapper">
+      <div class="hud_bottom_wrapper">
+        <div class="players_wrapper">
+          <PlayerAlive />
+          <PlayerAlive />
+          <PlayerAlive />
+          <PlayerAlive />
+          <PlayerAlive />
+        </div>
+      </div>  
+      </div>
+    </div>
+  </body>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
+
+<style>
+
+.players_wrapper {
+  grid-column-gap: 8px;
+  grid-row-gap: 8px;
+  flex-flow: row;
+  justify-content: flex-start;
+  align-items: flex-end;
+  display: flex;
+}
+
+.hud_bottom_wrapper {
+  width: 100%;
+  grid-column-gap: 64px;
+  grid-row-gap: 64px;
+  justify-content: center;
+  align-items: stretch;
+  display: flex;
+}
+
+.hud_top_wrapper {
+  width: 100%;
+  flex-flow: row;
+  justify-content: center;
+  align-items: flex-start;
+  display: flex;
+}
+
+.vita_v1 {
+  width: 100%;
+  height: 100vh;
+  max-height: 1080px;
+  max-width: 1920px;
+  flex-flow: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 37.5px 16px;
+  display: flex;
+  position: relative;
+}
+
+</style>
---
+++ src/components/overlay1/player/PlayerMainArea.vue
@@ -0,0 +1,102 @@
+<script setup>
+import PlayerImage from './PlayerImage.vue';
+import PlayerHPandArmor from './PlayerHPandArmor.vue';
+import PlayerName from './PlayerName.vue';
+import PlayerKills from './PlayerKills.vue';
+import PlayerDeaths from './PlayerDeaths.vue';
+import PlayerMoney from './PlayerMoney.vue';
+</script>
+
+<template>
+  <div class="player_hp_area_wrapper">
+    <div data-hudid="player_0_hp_bar" class="player_hp_bar"></div>
+      <PlayerImage />
+    <div data-hudid="player_0_hp_bar" class="player_hp_bar_gradient"></div>
+    <div class="player_hp_bar_info">
+      <PlayerHPandArmor />
+      <PlayerName />
+      <div class="player_stats_wrapper">
+        <PlayerKills />
+      </div>
+      <PlayerDeaths />
+      <PlayerMoney />
+    </div>
+  </div>
+</template>
+
+<style>
+
+.player_hp_area_wrapper {
+  width: 135px;
+  height: 100px;
+  grid-column-gap: 4px;
+  grid-row-gap: 4px;
+  background-color: var(--vita-v1-overlay-background);
+  border-radius: 8px;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 4px;
+  display: flex;
+  position: relative;
+  overflow: visible;
+}
+
+.player_hp_bar {
+  z-index: 1;
+  height: 100%;
+  background-color: var(--ov1_teamblue);
+  border-radius: 8px;
+  transition: all .5s cubic-bezier(.645, .045, .355, 1);
+  display: block;
+  position: absolute;
+  top: auto;
+  bottom: 0%;
+  left: 0%;
+  right: 0%;
+  overflow: hidden;
+}
+
+.player_hp_bar_gradient {
+  z-index: 3;
+  height: 100%;
+  background-image: linear-gradient(rgba(65, 126, 253, 0), rgba(65, 126, 253, .9) 31%, #417efd 54%);
+  border-radius: 8px;
+  transition: all .5s cubic-bezier(.645, .045, .355, 1);
+  position: absolute;
+  top: auto;
+  bottom: 0%;
+  left: 0%;
+  right: 0%;
+  overflow: hidden;
+}
+
+.player_hp_bar_gradient.is-t {
+  background-image: linear-gradient(rgba(255, 40, 92, 0), rgba(255, 40, 92, .9) 31%, #ff285c 54%);
+}
+
+.player_hp_bar_info {
+  z-index: 4;
+  width: 100%;
+  height: 100%;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+  top: 0%;
+  bottom: 0%;
+  left: 0%;
+  right: 0%;
+}
+
+.player_stats_wrapper {
+  grid-column-gap: 8px;
+  grid-row-gap: 8px;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+</style>
