ed7ad817a32c2f5cc13addcc6cfdb5c0c9403b99
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-24 23:04:27 +0100

    removed all

--- public/src/util/overlay1/index.js
+++
@@ -1,8 +0,0 @@
-import { overlay1store } from "public/src/stores/overlay1";
-
-const store = overlay1store();
-
-export function tester(json) {
-    let t_score = json["map"]["team_t"]["score"];
-    store.setTest(t_score)
-}
